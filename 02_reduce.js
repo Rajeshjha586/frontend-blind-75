6a69418086502a12b526377a567069ba13f78918
Rajesh Jha <jharajesh035@gmail.com>
2025-07-08 22:04:59 +0530

    feat: Problem 02 reduce

---
+++ 02_reduce.js
@@ -0,0 +1,25 @@
+/**
+ * @template T, U
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U} callbackFn
+ * @param {U} [initialValue]
+ * @return {U}
+ */
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  const hasInitialValue = arguments.length > 1;
+
+  if (!hasInitialValue && this.length === 0) {
+    throw new TypeError("Reduce of empty array with no initial value");
+  }
+
+  let accumulator = hasInitialValue ? initialValue : this[0];
+
+  for (let index = hasInitialValue ? 0 : 1; index < this.length; index++) {
+    if (index in this) {
+      accumulator = callbackFn(accumulator, this[index], index, this);
+    }
+  }
+
+  return accumulator;
+};
+
+console.log([1, 2, 3, , , , , , 5].myReduce((prev, curr) => prev + curr, 9));
