161b09c25a26596c3cc889054f56c0ca4a6c03ff
Rajesh Jha <jharajesh035@gmail.com>
2025-07-29 22:34:31 +0530

    feat: Problem 24 html serializer

---
+++ 24_html-serializer/html-serializer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Object} element
+ * @return {string}
+ */
+export default function serializeHTML(element, indent = "\t") {
+  function traverse(element, depth = 0) {
+    if (typeof element === "string") {
+      return `${indent.repeat(depth)}${element}`;
+    }
+
+    return [
+      `${indent.repeat(depth)}<${element.tag.toLowerCase()}>`,
+      ...element.children.flatMap((child) => traverse(child, depth + 1)),
+      `${indent.repeat(depth)}</${element.tag.toLowerCase()}>`,
+    ].join("\n");
+  }
+
+  return traverse(element);
+}
